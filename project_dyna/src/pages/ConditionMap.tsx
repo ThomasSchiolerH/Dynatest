7a90d13e713343cb698c14be49cdea932a1b68ea
ThomasSchiolerH <thomas@schioeler.com>
2023-10-10 11:32:14 +0200

    graph small name fix

--- project_dyna/src/pages/ConditionMap.tsx
+++ project_dyna/src/pages/ConditionMap.tsx
@@ -143,45 +143,45 @@ const getConditionColor = ( properties: GeoJSON.GeoJsonProperties) : string => {
 const data = [
     {
         name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        KPI: 4000,
+        DI: 2400,
+        IRI: 2400,
     },
     {
         name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        KPI: 3000,
+        DI: 1398,
+        IRI: 2210,
     },
     {
         name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        KPI: 2000,
+        DI: 9800,
+        IRI: 2290,
     },
     {
         name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        KPI: 2780,
+        DI: 3908,
+        IRI: 2000,
     },
     {
         name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        KPI: 1890,
+        DI: 4800,
+        IRI: 2181,
     },
     {
         name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
+        KPI: 2390,
+        DI: 3800,
+        IRI: 2500,
     },
     {
         name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
+        KPI: 3490,
+        DI: 4300,
+        IRI: 2100,
     },
 ];
 
@@ -201,7 +201,7 @@ const SlidingWindow = () => {
                 <div className="data-window-content">
                     <p>Data Window Content</p>
                     <div className={"chart-container"}>
-                        <h4>Graph 1</h4>
+                        <h4>Graph 1 with data type 1:</h4>
                         <ResponsiveContainer width="100%" height={200}>
                             <LineChart
                                 width={500}
@@ -219,10 +219,10 @@ const SlidingWindow = () => {
                                 <XAxis dataKey="name" />
                                 <YAxis />
                                 <Tooltip />
-                                <Line type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                                <Line type="monotone" dataKey="KPI" stroke="#8884d8" fill="#8884d8" />
                             </LineChart>
                         </ResponsiveContainer>
-                        <h4>Zoom graph below:</h4>
+                        <h4>Zoom graph with data type 2:</h4>
 
                         <ResponsiveContainer width="100%" height={200}>
                             <LineChart
@@ -241,11 +241,11 @@ const SlidingWindow = () => {
                                 <XAxis dataKey="name" />
                                 <YAxis />
                                 <Tooltip />
-                                <Line type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
+                                <Line type="monotone" dataKey="DI" stroke="#82ca9d" fill="#82ca9d" />
                                 <Brush />
                             </LineChart>
                         </ResponsiveContainer>
-                        <h4>Filled graph:</h4>
+                        <h4>Graph with data type 3:</h4>
 
                         <ResponsiveContainer width="100%" height={200}>
                             <AreaChart
@@ -264,7 +264,7 @@ const SlidingWindow = () => {
                                 <XAxis dataKey="name" />
                                 <YAxis />
                                 <Tooltip />
-                                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
+                                <Area type="monotone" dataKey="IRI" stroke="#82ca9d" fill="#82ca9d" />
                             </AreaChart>
                         </ResponsiveContainer>
                     </div>
