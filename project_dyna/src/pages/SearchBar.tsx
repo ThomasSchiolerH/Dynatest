ce7b14c58ee1e434a0c0e3cf7b3c1b87357c3180
ThomasSchiolerH <thomas@schioeler.com>
2023-11-20 17:14:43 +0100

    search bar tooltip created and resize icons

--- project_dyna/src/pages/SearchBar.tsx
+++ project_dyna/src/pages/SearchBar.tsx
@@ -13,27 +13,32 @@ const SearchBar = () => {
 
     const SearchIcon = () => {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style={{ fill: 'rgba(0, 0, 0, 1)' }}>
-                <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
-            </svg>
+            <div className="search-icon-container">
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" style={{ fill: 'rgba(105, 105, 105, 1)' }}>
+                    <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
+                </svg>
+                <span className="search-tooltip">Search</span>
+            </div>
         );
     };
 
     const CloseIcon = () => {
         return (
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                style={{
-                    fill: "rgba(119, 118, 118, 0.72)",
-                    transform: "scaleY(-1)",
-                    msFilter: "progid:DXImageTransform.Microsoft.BasicImage(rotation=2, mirror=1)"
-                }}
-            >
-                <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4.207 12.793-1.414 1.414L12 13.414l-2.793 2.793-1.414-1.414L10.586 12 7.793 9.207l1.414-1.414L12 10.586l2.793-2.793 1.414 1.414L13.414 12l2.793 2.793z"></path>
-            </svg>
+            <div className="close-icon-container">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                    style={{
+                        fill: "rgba(119, 118, 118, 0.72)",
+                        transform: "scaleY(-1)",
+                        msFilter: "progid:DXImageTransform.Microsoft.BasicImage(rotation=2, mirror=1)"
+                    }}
+                >
+                    <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4.207 12.793-1.414 1.414L12 13.414l-2.793 2.793-1.414-1.414L10.586 12 7.793 9.207l1.414-1.414L12 10.586l2.793-2.793 1.414 1.414L13.414 12l2.793 2.793z"></path>
+                </svg>
+            </div>
         );
     };
     useEffect(() => {
@@ -141,6 +146,7 @@ const SearchBar = () => {
                 className="input"
                 placeholder="Search"
                 required
+                title="Write the road name that you want to find."
                 type="text"
                 value={searchQuery}
                 onChange={handleInputChange}
